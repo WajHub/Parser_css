ff6fcaf28e5827e27b352b789153e76d7a9f4419
WajdaHub <hubert.wajda03@gmail.com>
2023-03-27 01:28:39 +0200

    Creating list

--- List.cpp
+++ List.cpp
@@ -1,5 +1,20 @@
 //
 // Created by huber on 23.03.2023.
 //
-
 #include "List.h"
+
+template<typename T>
+List<T>::List() {
+    head = nullptr;
+    tail = nullptr;
+}
+
+template<typename T>
+void List<T>::push(T obj) {
+    Node<T> *n = new Node<T>();
+}
+
+template<typename T>
+void List<T>::print() {
+
+}
--- List.h
+++ List.h
@@ -4,11 +4,35 @@
 
 #ifndef PARSER_CSS_LIST_H
 #define PARSER_CSS_LIST_H
-#define LENGTH = 8
 
-class List {
+#define LENGTH  8
+template <typename T>
+class Node {
+    T array[LENGTH];
+    int counter;
+    Node *next;
+    Node *prev;
+public:
+    Node(){
+        counter=0;
+        next=0;
+        prev=0;
+    }
+};
+
+
+template <typename T> class List {
+private:
+    Node<T> *head;
+    Node<T> *tail;
+public:
+    List();
+    void push(T obj);
+    void print();
 
 };
+
+
 
 
 #endif //PARSER_CSS_LIST_H
